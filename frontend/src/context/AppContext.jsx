7fa6a71847d74b3462d2be7155709e0b172b6d0f
nikhilgetitdone2027 <cracksdenow12345@gmail.com>
2026-04-28 02:21:11 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,33 +10,29 @@ const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors, setDoctors] = useState([])
-    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
+    
+    
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
     const [userData, setUserData] = useState(false)
 
-    // Getting Doctors using API
+    
     const getDoctosData = async () => {
-
         try {
-
             const { data } = await axios.get(backendUrl + '/api/doctor/list')
             if (data.success) {
                 setDoctors(data.doctors)
             } else {
                 toast.error(data.message)
             }
-
         } catch (error) {
             console.log(error)
             toast.error(error.message)
         }
-
     }
 
-    // Getting User Profile using API
+    
     const loadUserProfileData = async () => {
-
         try {
-
             const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
 
             if (data.success) {
@@ -44,21 +40,27 @@ const AppContextProvider = (props) => {
             } else {
                 toast.error(data.message)
             }
-
         } catch (error) {
             console.log(error)
+            
+            if (error.response?.data?.message === "Invalid Signature" || error.response?.status === 401) {
+                setToken(false)
+                localStorage.removeItem('token')
+            }
             toast.error(error.message)
         }
-
     }
 
     useEffect(() => {
         getDoctosData()
     }, [])
 
+    
     useEffect(() => {
         if (token) {
             loadUserProfileData()
+        } else {
+            setUserData(false)
         }
     }, [token])
 
@@ -75,7 +77,6 @@ const AppContextProvider = (props) => {
             {props.children}
         </AppContext.Provider>
     )
-
 }
 
 export default AppContextProvider
