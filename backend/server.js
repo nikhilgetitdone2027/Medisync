2dc956915e6d9031e43c809703508e836cf342d1
nikhilgetitdone2027 <cracksdenow12345@gmail.com>
2026-04-28 02:00:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,16 @@ import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
 
-// app config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// middlewares
+
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
+
 app.use("/api/user", userRouter)
 app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
